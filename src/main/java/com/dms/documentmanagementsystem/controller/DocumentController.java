da204301d88e22548ebfea6cd9905eca13486c6b
theladykiller <niki.plachetzky@gmail.com>
2025-09-28 17:04:01 +0200

    fixed some HTTP status codes in the controller

--- src/main/java/com/dms/documentmanagementsystem/controller/DocumentController.java
+++ src/main/java/com/dms/documentmanagementsystem/controller/DocumentController.java
@@ -5,22 +5,38 @@ import com.dms.documentmanagementsystem.service.DocumentService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/documents")
 public class DocumentController {
     private final DocumentService service;
-
     public DocumentController(DocumentService service) {
         this.service = service;
     }
 
+    /**
+     * Save a Document
+     *
+     * @param doc the document to be saved
+     * @return saved document with location
+     */
     @PostMapping
-    public Document upload(@RequestBody Document doc) {
-        return service.saveDocument(doc);
+    public ResponseEntity<Document> upload(@RequestBody Document doc) {
+        var saved = service.saveDocument(doc);
+        return ResponseEntity
+                .created(URI.create("/api/documents/" + saved.getId()))
+                .body(saved);
     }
 
+    /**
+     * search for a Document
+     *
+     * @param id the document id
+     * @return found document
+     *         or 404 Not Found if non-existent param
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Document> get(@PathVariable Long id) {
         return service.getDocument(id)
@@ -33,8 +49,16 @@ public class DocumentController {
         return service.getAllDocuments();
     }
 
+    /**
+     * delete a specific Document
+     *
+     * @param id the document id
+     * @return 204 No Content if deleted & if Document never existed
+     *         or 404 Not Found if non-numeric param
+     */
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.deleteDocument(id);
+        return ResponseEntity.noContent().build();
     }
 }
