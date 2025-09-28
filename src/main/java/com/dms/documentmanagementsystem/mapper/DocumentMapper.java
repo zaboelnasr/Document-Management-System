ffc3a5f58f3875c703844b45a7e470dba65fcba8
theladykiller <niki.plachetzky@gmail.com>
2025-09-28 20:50:38 +0200

    added draft Document Mapper

--- src/main/java/com/dms/documentmanagementsystem/mapper/DocumentMapper.java
+++ src/main/java/com/dms/documentmanagementsystem/mapper/DocumentMapper.java
@@ -1,4 +1,26 @@
 package com.dms.documentmanagementsystem.mapper;
 
+import com.dms.documentmanagementsystem.dto.DocumentRequestDTO;
+import com.dms.documentmanagementsystem.dto.DocumentResponseDTO;
+import com.dms.documentmanagementsystem.model.Document;
+
 public class DocumentMapper {
+    private DocumentMapper() {}
+
+    public static Document toEntity(DocumentRequestDTO dto) {
+        Document d = new Document();
+        d.setFileName(dto.getFileName());
+        d.setSummary(dto.getSummary());
+        return d;
+    }
+
+    public static DocumentResponseDTO toDTO(Document entity) {
+        DocumentResponseDTO dto = new DocumentResponseDTO();
+        dto.setId(entity.getId());
+        dto.setFileName(entity.getFileName());
+        dto.setSummary(entity.getSummary());
+        dto.setCreatedAt(entity.getCreatedAt());
+        dto.setUpdatedAt(entity.getUpdatedAt());
+        return dto;
+    }
 }
