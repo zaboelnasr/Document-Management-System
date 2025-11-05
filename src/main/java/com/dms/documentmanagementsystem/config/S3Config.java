c53bb6a536121957d2c4cc8d93273989ede1e4fa
theladykiller <niki.plachetzky@gmail.com>
2025-11-05 15:34:06 +0100

    added new S3Config.java to both the backend(src/ dir) and the ocr worker

---
+++ src/main/java/com/dms/documentmanagementsystem/config/S3Config.java
@@ -0,0 +1,31 @@
+package com.dms.documentmanagementsystem.config;
+
+import java.net.URI;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+
+    @Value("${s3.endpoint}") private URI endpoint;
+    @Value("${s3.region:us-east-1}") private String region;
+    @Value("${s3.accessKey}") private String accessKey;
+    @Value("${s3.secretKey}") private String secretKey;
+    @Value("${s3.pathStyleAccess:true}") private boolean pathStyle;
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .endpointOverride(endpoint)     // MinIO endpoint
+                .region(Region.of(region))
+                .forcePathStyle(pathStyle)      // REQUIRED for MinIO
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey, secretKey)))
+                .build();
+    }
+}
